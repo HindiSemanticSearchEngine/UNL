df6b8f53ba176aec2846bbdf7ebc4497e3ce9077
dushyant7917 <dushyant7917@gmail.com>
2017-07-29 23:44:08 +0530

    new rules made by ritvik and sid added

--- relation.py
+++ relation.py
@@ -46,9 +46,38 @@ class Relations:
         else:
             return False
 
+    def aoj_relation(self, w1, w2):
+        if w1['pos_tag'] == 'NNP' and w2['pos_tag'] == 'PRP':
+            return True
+        elif w1['pos_tag'] == 'PRP' and w2['pos_tag'] == 'NN':
+            return True
+        elif w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'PRP':
+            return True
+        elif w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'JJ':
+            return True
+        else:
+            return False
 
-    '''
-    def and_relation(self, w1, w2):
-        if w1['nxt'] == 'CC' and w2['prv'] == 'CC':
-            if w1['pos_tag'] == 'NN' and w2['']
-    '''
+    def and_relation(self, w1, w2, w3):
+        if w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'CC' and  w3['pos_tag'] == 'NN':
+            return True
+        elif w1['pos_tag'] == 'NNP' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'NNP':
+            return True
+        elif w1['pos_tag'] == 'RB' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'RB':
+            return True
+        elif w1['pos_tag'] == 'JJ' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'JJ':
+            return True
+        else:
+            return False
+
+    def nam_relation(self, w1, w2):
+        if w1['pos_tag'] == 'NN' and  w2['pos_tag'] == 'NNP':
+            return True
+        elif w1['pos_tag'] == 'NNP' and w2['pos_tag'] == 'NN':
+            return True
+        elif w1['pos_tag'] == 'PRP' and w2['pos_tag'] == 'NNP':
+            return True
+        elif w1['pos_tag'] == 'NNP' and w2['pos_tag'] == 'PRP':
+            return True
+        else:
+            return False
