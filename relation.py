519198f7614cdba8555ce96d1a70d6fefc1e810b
Jeevanshu Malhotra <jeevanshumalhotra@gmail.com>
2017-07-30 14:49:59 +0530

    Update relation.py

--- relation.py
+++ relation.py
@@ -140,3 +140,24 @@ class Relations:
 
         else:
             return False
+        
+    def or_relation(self, w1, w2, w3):
+        if w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'CC' and  w3['pos_tag'] == 'NN':
+            return True
+
+        elif w1['pos_tag'] == 'NNP' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'NNP':
+            return True
+
+        elif w1['pos_tag'] == 'RB' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'RB':
+            return True
+
+        elif w1['pos_tag'] == 'JJ' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'JJ':
+            return True
+
+        elif w1['pos_tag'] == 'INJ' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'NEG':
+            return True
+
+        elif w1['pos_tag'] == 'VV' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'JJ':
+            return True
+        else:
+            return False
