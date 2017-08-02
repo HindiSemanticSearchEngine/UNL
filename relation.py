82f6ae3042b0115e830673c0be42001764dc22de
dushyant7917 <dushyant7917@gmail.com>
2017-08-02 10:28:33 +0530

    ant and or relation added

--- relation.py
+++ relation.py
@@ -1,6 +1,7 @@
 class Relations:
     '"Class for identifying unl relation between two words"'
 
+
     def qua_relation(self, w1, w2):
     	if w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'QF':
     		return True
@@ -14,6 +15,7 @@ class Relations:
     	else:
     		return False
 
+
     def dur_relation(self, w1, w2):
         if w1['pos_tag'] == 'VM' and w2['pos_tag'] == 'QF':
             return True
@@ -23,6 +25,7 @@ class Relations:
 
         else:
             return False
+
 
     def agt_relation(self, w1, w2):
         if w1['pos_tag'] == 'VM' and w2['pos_tag']=='NN':
@@ -46,6 +49,7 @@ class Relations:
         else:
             return False
 
+
     def aoj_relation(self, w1, w2):
         if w1['pos_tag'] == 'NNP' and w2['pos_tag'] == 'PRP':
             return True
@@ -61,6 +65,7 @@ class Relations:
 
         else:
             return False
+
 
     def and_relation(self, w1, w2, w3):
         if w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'CC' and  w3['pos_tag'] == 'NN':
@@ -78,6 +83,7 @@ class Relations:
         else:
             return False
 
+
     def nam_relation(self, w1, w2):
         if w1['pos_tag'] == 'NN' and  w2['pos_tag'] == 'NNP':
             return True
@@ -94,8 +100,9 @@ class Relations:
         else:
             return False
 
+
     def obj_relation(self, w1, w2):
-        if w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'VM:
+        if w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'VM':
     		return True
 
     	elif w1['pos_tag'] == 'VM' and w2['pos_tag'] == 'NN':
@@ -115,3 +122,46 @@ class Relations:
 
     	else:
     		return False
+
+
+    def ant_relation(self, sentence):
+        if 'नहीं' not in l:
+            return False
+
+        for i in l:
+            for j in l:
+                if i!=j:
+                    if i['pos_tag'] == 'NN' and j['pos_tag'] == 'NN':
+                        return True
+
+                    elif i['pos_tag'] == 'PRP' and j['pos_tag'] == 'PRP':
+                        return True
+
+                    elif (i['pos_tag'] == 'JJ' and j['pos_tag'] == 'JJ':
+                        return True
+
+                    else:
+                        return False
+
+
+    def or_relation(self, w1, w2, w3):
+        if w1['pos_tag'] == 'NN' and w2['pos_tag'] == 'CC' and  w3['pos_tag'] == 'NN':
+            return True
+
+        elif w1['pos_tag'] == 'NNP' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'NNP':
+            return True
+
+        elif w1['pos_tag'] == 'RB' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'RB':
+            return True
+
+        elif w1['pos_tag'] == 'JJ' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'JJ':
+            return True
+
+        elif w1['pos_tag'] == 'INJ' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'NEG':
+            return True
+
+        elif w1['pos_tag'] == 'VV' and w2['pos_tag'] == 'CC' and w3['pos_tag'] == 'JJ':
+            return True
+
+        else:
+            return False
